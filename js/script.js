9590efdc493cc1a3bed787a5fe3f5ed027578deb
zaturnvalley <zacharycmason@gmail.com>
2016-12-28 13:11:05 -0800

    added comments

--- js/script.js
+++ js/script.js
@@ -4,9 +4,16 @@ var speed = "500";
 
 $(document).ready(function() {
   $('li.q').on(action, function() {
+    // Gets next element
     $(this).next()
         .slideToggle(speed)
+          // Select all other answers
           .siblings('li.a')
             .slideUp();
+  // Get active arrow quesetion
+  var img = $(this).children('img');
+  // This will remove the 'rotate' class
+  // for all images except active
+  $()
   });
 });
